3c3bc6eeae8c02ff865891760e6276bfe0164ea8
Artem Glazychev <artem.glazychev@xored.com>
2024-03-20 17:37:31 +0700

    test
    Signed-off-by: Artem Glazychev <artem.glazychev@xored.com>

--- scripts/setup-actions-runner.sh
+++ scripts/setup-actions-runner.sh
@@ -4,8 +4,8 @@
 runner_token=$1
 
 mkdir actions-runner && cd actions-runner
-curl -o actions-runner-linux-x64-2.305.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.305.0/actions-runner-linux-x64-2.305.0.tar.gz
-tar xzf ./actions-runner-linux-x64-2.305.0.tar.gz
+curl -o actions-runner-linux-x64-2.314.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.314.1/actions-runner-linux-x64-2.314.1.tar.gz
+tar xzf ./actions-runner-linux-x64-2.314.1.tar.gz
 
 export RUNNER_ALLOW_RUNASROOT=1
 echo | ./config.sh --url https://github.com/networkservicemesh/govpp --token ${runner_token} || exit 1
